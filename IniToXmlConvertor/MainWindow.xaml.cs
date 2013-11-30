54fb1bf3cba1fee1f56ae780fd27a9aebfe06d49
Rositsa Yordanova <riordanova@gmail.com>
2013-11-30 20:09:56 +0200

    Initial Commit

---
+++ IniToXmlConvertor/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+using System.Xml.Linq;
+using IniToXmlModel;
+
+namespace IniToXmlConvertor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonLoadIniFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialogLoad = new OpenFileDialog();
+            openFileDialogLoad.Filter = "Ini files (.ini)|*.ini";
+            openFileDialogLoad.FilterIndex = 1;
+            openFileDialogLoad.Multiselect = false;
+
+            if (openFileDialogLoad.ShowDialog() == true)
+            {
+                textBoxLoadIniFile.Text = openFileDialogLoad.FileName;
+                textBlockIniContent.Text = IniToXmlModel.IniToXmlModel.ReadEntireFile(openFileDialogLoad.FileName);
+            }
+        }
+
+        private void buttonLoadXmlFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialogLoad = new OpenFileDialog();
+            openFileDialogLoad.Filter = "Xml files (.xml)|*.xml";
+            openFileDialogLoad.FilterIndex = 1;
+            openFileDialogLoad.Multiselect = false;
+
+            if (openFileDialogLoad.ShowDialog() == true)
+            {
+                textBoxLoadXmlFile.Text = openFileDialogLoad.FileName;
+                
+            }
+        }
+
+        private void buttonConvert_Click(object sender, RoutedEventArgs e)
+        {
+            // check if the output xml file exists
+            if (!File.Exists(textBoxLoadXmlFile.Text))
+            {
+                MessageBox.Show(Properties.Resources.MissingXmlFile, Properties.Resources.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var outputFile = textBoxLoadXmlFile.Text;
+
+            // check if the input ini file exists
+            if (!File.Exists(textBoxLoadIniFile.Text))
+            {
+                MessageBox.Show(Properties.Resources.MissingIniFile, Properties.Resources.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var inputFile = textBoxLoadIniFile.Text;
+            string appRootEl = Properties.Resources.MyAppRoot;
+
+            // check if root value is inserted and change if this is the case
+            if (!textBoxAppRootElement.Text.Equals(Properties.Resources.RootElement))
+            {
+                appRootEl = textBoxAppRootElement.Text;
+            }
+
+            try
+            {
+                XDocument doc = IniToXmlModel.IniToXmlModel.ConvertIni(inputFile, appRootEl);
+                doc.Save(outputFile);
+
+                textBlockXmlContent.Text = IniToXmlModel.IniToXmlModel.ReadEntireFile(outputFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Properties.Resources.Error, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
---
+++ IniToXmlModel/IniToXmlModel.cs
@@ -0,0 +1,83 @@
+﻿namespace IniToXmlModel
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+    using System.Xml.Linq;
+    using System.IO;
+
+    public class IniToXmlModel
+    {
+        /// <summary>
+        /// Converts the ini file to xml
+        /// </summary>
+        /// <param name="filePath">The file path of the ini file</param>
+        /// <param name="appRootElement">The root element of the new xml example My App Root</param>
+        /// <returns>Returns XDocument</returns>
+        public static XDocument ConvertIni(string filePath, string appRootElement)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            StringBuilder currentLine = new StringBuilder();
+            //Creating our document
+            XDocument doc = new XDocument(
+            new XDeclaration("1.0", "utf-8", "yes"),
+            new XElement(appRootElement));
+
+            //creating a xelement to attach the root
+            XElement root = new XElement("root");
+
+            //start reading the ini file
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (line.Contains("["))
+                    {
+                        currentLine.Append(line.Substring(1, line.Length - 2));
+                        root = new XElement(currentLine.ToString());
+                        doc.Root.Add(root);
+                        currentLine.Clear();
+                    }
+                    else if (line.Length != 0)
+                    {
+                        string[] values = line.Split('=');
+                        root.Add(new XElement(root.Name,
+                            new XAttribute("Name", values[0]),
+                            new XAttribute("Value", values[1])));
+                    }
+                    line = reader.ReadLine();
+
+                }
+            }
+            return doc;
+        }
+
+        /// <summary>
+        /// Reads all lines in file and add \n to format each line
+        /// </summary>
+        /// <param name="filePath">Path of the file to be read</param>
+        /// <returns>String with the content of the file</returns>
+        public static string ReadEntireFile(string filePath)
+        {
+            StringBuilder fileText = new StringBuilder();
+
+            try
+            {
+                string[] linesFile = File.ReadAllLines(filePath);
+
+                foreach (var line in linesFile)
+                {
+                    fileText.Append(line);
+                    fileText.Append("\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return fileText.ToString();
+        }
+    }
+}
